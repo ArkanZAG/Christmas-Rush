8fc04ab8311de0890ec3e67345782249f6e55f99
ArkanZAG <arkanalghifari22@gmail.com>
2023-12-17 15:49:32 +0700

    Player Movement

--- Assets/Scripts/Player/PlayerMechanic.cs
+++ Assets/Scripts/Player/PlayerMechanic.cs
@@ -7,6 +7,7 @@ public class PlayerMechanic : MonoBehaviour
     [SerializeField] private Rigidbody2D playerRigidBody;
     [SerializeField] private int playerMoveSpeed;
     [SerializeField] private int playerJumpHeight;
+    [SerializeField] private Animator playerAnimator;
     void Start()
     {
         
@@ -14,12 +15,25 @@ public class PlayerMechanic : MonoBehaviour
     
     void Update()
     {
-        playerRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * playerMoveSpeed, playerRigidBody.velocity.y);
-
+        float horizontalInput = Input.GetAxis("Horizontal");
+        
+        playerRigidBody.velocity = new Vector2(horizontalInput * playerMoveSpeed, playerRigidBody.velocity.y);
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRigidBody.velocity =
                 new Vector2(playerRigidBody.velocity.x, playerJumpHeight);
         }
+        
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        playerAnimator.SetBool("Run", horizontalInput != 0);
     }
 }
