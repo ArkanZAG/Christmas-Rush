df900894ad2d98508319b178b81624575c16f44e
ArkanZAG <arkanalghifari22@gmail.com>
2023-12-17 19:59:09 +0700

    Jumping Animation

--- Assets/Scripts/Player/PlayerMechanic.cs
+++ Assets/Scripts/Player/PlayerMechanic.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMechanic : MonoBehaviour
@@ -8,6 +10,7 @@ public class PlayerMechanic : MonoBehaviour
     [SerializeField] private int playerMoveSpeed;
     [SerializeField] private int playerJumpHeight;
     [SerializeField] private Animator playerAnimator;
+    private bool isGrounded;
     void Start()
     {
         
@@ -19,12 +22,6 @@ public class PlayerMechanic : MonoBehaviour
         
         playerRigidBody.velocity = new Vector2(horizontalInput * playerMoveSpeed, playerRigidBody.velocity.y);
         
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerRigidBody.velocity =
-                new Vector2(playerRigidBody.velocity.x, playerJumpHeight);
-        }
-        
         if (horizontalInput > 0.01f)
         {
             transform.localScale = Vector3.one;
@@ -34,6 +31,26 @@ public class PlayerMechanic : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
         
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
         playerAnimator.SetBool("Run", horizontalInput != 0);
+        playerAnimator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Jump()
+    {
+        playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, playerJumpHeight);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
